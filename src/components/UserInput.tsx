6316c888242a7ba7785861fe23c7eb09ed280ff7
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-06-29 22:12:46 +0900

    Feat : useState 훅을 활용하여 UserInput 이벤트 처리 로직 구현

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import { FiPlusCircle, FiArrowUpCircle } from "react-icons/fi";
 
 const UserInput = () => {
+	const [userInputValue, setUserInputValue] = useState("");
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setUserInputValue("");
+	};
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = e.currentTarget;
+		setUserInputValue(value);
+	};
+
 	return (
-		<form className="w-full flex items-center justify-center bg-beige h-14 px-4 py-2 gap-2">
+		<form
+			className="w-full flex items-center justify-center bg-beige h-14 px-4 py-2 gap-2"
+			action="submit"
+			onSubmit={handleSubmit}
+		>
 			<FiPlusCircle className="flex-none text-3xl text-gray" />
 			<div className="relative flex-auto w-full flex items-center">
 				<label className="sr-only">userInput</label>
-				<input className="w-full py-2 px-4 text-sm border-2 rounded-2xl border-beige outline-none placeholder:text-xs " />
-				<FiArrowUpCircle className="absolute right-3 text-3xl text-white fill-main" />
+				<input
+					name="user"
+					value={userInputValue}
+					placeholder="메세지를 입력해주세요"
+					className="w-full py-2 px-4 text-sm border-2 rounded-2xl border-beige outline-none placeholder:text-xs "
+					onChange={handleChange}
+					autoComplete="on"
+				/>
+				{!!userInputValue && (
+					<FiArrowUpCircle className="absolute right-3 text-3xl text-white fill-main" />
+				)}
 			</div>
 		</form>
 	);
